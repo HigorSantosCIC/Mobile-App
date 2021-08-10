5b2bc2a4c9086719a049ad9ebce474f47102c20d
Higor Gabriel <38538335+HigorSantosCIC@users.noreply.github.com>
2021-08-10 13:49:15 -0300

    Update src/components/Button/index.js
    Children instead string
    
    Co-authored-by: Pedro Augusto Ramalho Duarte <43936240+PedroAugustoRamalhoDuarte@users.noreply.github.com>

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,11 +3,11 @@ import { TouchableWithoutFeedback } from 'react-native';
 import Typography from '../Typography';
 import { Button } from './styles';
 
-const ButtonMain = ({ handlePress, color, styleTypho, string }) => {
+const ButtonMain = ({ handlePress, color, styleTypho, children }) => {
   return (
     <TouchableWithoutFeedback onPress={handlePress}>
       <Button color={color}>
-        <Typography style={styleTypho}>{string}</Typography>
+        <Typography style={styleTypho}>{children}</Typography>
       </Button>
     </TouchableWithoutFeedback>
   );
