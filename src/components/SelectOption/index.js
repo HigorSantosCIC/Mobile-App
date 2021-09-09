dfe8cabb005fbeec2716fa5394e644bc168be355
artur zorron <tucazorron@gmail.com>
2021-09-09 01:24:54 -0300

    remove radio buttons data console log

--- src/components/SelectOption/index.js
+++ src/components/SelectOption/index.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import RadioGroup from 'react-native-radio-buttons-group';
 
 const SelectOption = ({ radioButtonsData }) => {
-  console.log(radioButtonsData);
   const [radioButtons, setRadioButtons] = useState(radioButtonsData);
 
   function onPressRadioButton(radioButtonsArray) {
