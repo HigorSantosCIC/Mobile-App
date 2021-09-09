1b6fbc81f4aed3e568d4ba3449d82325332cacee
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2021-09-08 21:40:02 -0300

    My animals screen

--- src/screens/DashboardScreen/index.js
+++ src/screens/DashboardScreen/index.js
@@ -1,13 +1,10 @@
 import { View } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import api from '../../services/api';
-import firebase from 'firebase';
+import React from 'react';
 import Button from '../../components/Button';
 import { dp } from '../../constants/Spacing';
 import { theme } from '../../constants/Theme';
 
 const DashboardScreen = () => {
-
   return (
     <View style={{ flexGrow: 1, backgroundColor: '#FAFAFA' }}>
       <View
@@ -21,13 +18,6 @@ const DashboardScreen = () => {
           onPress={() => navigation.push('Adopt')}>
           Adotar
         </Button>
-        <View style={{ paddingTop: dp(12) }}>
-          <Button
-            color={theme.colors.secondary}
-            onPress={() => console.log('Em breve')}>
-            Ajudar
-          </Button>
-        </View>
         <View style={{ paddingTop: dp(12) }}>
           <Button
             color={theme.colors.secondary}
