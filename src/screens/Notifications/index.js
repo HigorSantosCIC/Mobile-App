54c9c9d5c7c828b335e5a9a50e85165cea10debc
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2021-09-20 12:45:32 -0300

    Fixes eslint

--- src/screens/Notifications/index.js
+++ src/screens/Notifications/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import api from '../../services/api';
 import firebase from 'firebase';
 import { ActivityIndicator, FlatList, View } from 'react-native';
