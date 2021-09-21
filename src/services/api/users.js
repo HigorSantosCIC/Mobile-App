c9956ffdc1b8cccf9705ef353353fa8c1229f2f5
artur zorron <tucazorron@gmail.com>
2021-09-21 01:56:27 -0300

    fix getAdress function

--- src/services/api/users.js
+++ src/services/api/users.js
@@ -52,18 +52,28 @@ export const show = async (id) => {
   });
 };
 
-export const getAdress = async (id) => {
-  const db = firebase.firestore();
-  const user = await db.collection('users').doc(id).get();
-  console.log('>> user');
-  console.log(user);
-  return user.address;
+export const getAddress = async (id) => {
+  return new Promise((resolve, reject) => {
+    firebase
+      .firestore()
+      .collection('users')
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (!doc.exists) {
+          Alert.alert('No owner data found!');
+          reject('No owner data found!');
+        } else {
+          resolve(doc.data().address);
+        }
+      });
+  });
 };
 
 const Users = {
   create,
   show,
-  getAdress,
+  getAddress,
 };
 
 export default Users;
