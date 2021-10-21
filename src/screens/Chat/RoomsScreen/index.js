d1b5e1001557762df11fb7a6dd3f06ba7b6513d2
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2021-10-20 23:54:48 -0300

    Initialize RoomsScreen code

--- src/screens/Chat/RoomsScreen/index.js
+++ src/screens/Chat/RoomsScreen/index.js
@@ -1,8 +1,71 @@
-import { View } from 'react-native';
-import React from 'react';
+import { FlatList, View, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import firebase from 'firebase';
+import useAuth from '../../../hooks/useAuth';
+import { Divider, List } from 'react-native-paper';
 
-const RoomScreen = () => {
-  return <View style={{ flexGrow: 1, backgroundColor: '#FAFAFA' }}></View>;
+const RoomsScreen = ({ navigation }) => {
+  const db = firebase.firestore();
+  const { user } = useAuth();
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    // Get users rooms
+    let fetchRooms = [];
+    db.collection('rooms')
+      .where('user1', '==', user.uid)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          fetchRooms.append(doc.data());
+        });
+      });
+    db.collection('rooms')
+      .where('user2', '==', user.uid)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          fetchRooms.append(doc.data());
+        });
+      });
+    setRooms(fetchRooms);
+  });
+
+  return (
+    <View style={{ flexGrow: 1, backgroundColor: '#FAFAFA' }}>
+      <FlatList
+        data={rooms}
+        keyExtractor={(item) => item._id}
+        ItemSeparatorComponent={() => <Divider />}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Room', { room: item })}>
+            <List.Item
+              title={item.name}
+              // description={item.latestMessage.text}
+              titleNumberOfLines={1}
+              titleStyle={styles.listTitle}
+              descriptionStyle={styles.listDescription}
+              descriptionNumberOfLines={1}
+            />
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
 };
 
-export default RoomScreen;
+export default RoomsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f5f5f5',
+    flex: 1,
+  },
+  listTitle: {
+    fontSize: 22,
+  },
+  listDescription: {
+    fontSize: 16,
+  },
+});
